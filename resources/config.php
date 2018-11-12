d313a7b5ff7ae9232c8c49e442c126c48ae41783
Nathanmalnoury <36629250+nathanmalnoury@users.noreply.github.com>
2018-11-12 18:01:32 +0100

    Premiere avancee : / Ajax charge les commentaires en loadant la page / functionnalité triggered par appui sur un bouton

--- resources/config.php
+++ resources/config.php
@@ -1,25 +1,12 @@
 <?php
 
 
-/* ACCES A P5 : 
-$BDD_host="ig2ip5.priv.ig2i.fr";
-$BDD_user="chat"; 
-$BDD_password="chat"; 
-$BDD_base="paragraphes";
-*/
-
-/* MACHINES IG2I : 
-$BDD_host="localhost";
-$BDD_user="administrateur"; 
-$BDD_password=""; // pas de mot de passe 
-$BDD_base="paragraphes";
-*/
 
 
 // MACHINE LINUX 
 $BDD_host="localhost";
 $BDD_user="root";
 $BDD_password="root";//à changer pour WAMP => mysql
-$BDD_base="paragraphes";
+$BDD_base="exercices";
 
 ?>
